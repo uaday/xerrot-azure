b869d666f0aac9b564ab15f945326bd10a6f22cc
sudipta ghosh <uadayghosh@gmail.com>
2020-01-18 20:04:02 +0600

    azure yanl upadated

--- AKS/Services/HelloNode/server.js
+++ AKS/Services/HelloNode/server.js
@@ -1,10 +1,12 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 50002;
+"use strict";
+var http = require("http");
+var port = process.env.PORT || 30002;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('{ "message": "Hello World from Node.js", "port": ' + port + ' }');
-}).listen(port, _cb => {
-    console.log('started on port: ' + port);
-});
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end('{ "message": "Hello World from Node.js", "port": ' + port + " }");
+  })
+  .listen(port, _cb => {
+    console.log("started on port: " + port);
+  });
