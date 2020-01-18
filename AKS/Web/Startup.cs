157bc9b3346ee141751b7ac51727113633c950d4
sudipta ghosh <uadayghosh@gmail.com>
2020-01-18 20:48:10 +0600

    azure web ready

--- AKS/Web/Startup.cs
+++ AKS/Web/Startup.cs
@@ -19,7 +19,7 @@ namespace Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddControllersWithViews();
+            services.AddControllers();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
